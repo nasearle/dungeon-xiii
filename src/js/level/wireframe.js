165eaa61a7137150cb6b8ccb02e0d6c4c9b56d7a
nasearle <nicolasasearle@gmail.com>
2024-09-05 20:03:22 -0700

    Adds light effect
    - wireframe.js converts the map collision layer data to lines and points
    - light.js uses the wireframe to draw rays and find intersect points to
    draw a light polygon on the player

---
+++ src/js/level/wireframe.js
@@ -0,0 +1,169 @@
+import { mapData } from "./mapData";
+import { convertArrayToMatrix } from "../util/util";
+
+class Wireframe {
+  constructor() {
+    this.points = [
+      {x: 0, y: 0},
+      {x: mapData.canvasWidth, y: 0},
+      {x: 0, y: mapData.canvasHeight},
+      {x: mapData.canvasWidth, y: mapData.canvasHeight}
+    ];
+    this.lines = [];
+
+    const layerData = this.getLayerData('collision');
+    this.getHorizontalsFromLayer(layerData);
+    this.getVerticalsFromLayer(layerData);
+  }
+
+  getLayerData(layerName) {
+    return mapData.layers.filter((elem) => elem.name == layerName)[0].data;
+  }
+
+  addPoint(point) {
+    if (!this.points.includes(point)) {
+      this.points.push(point)
+    }
+  }
+
+  addLine(line) {
+    this.lines.push(line);
+  }
+
+  getBoxPoints(left, right, top, bottom) {
+    return {
+      topLeft: {x: left, y: top},
+      topRight: {x: right, y: top},
+      bottomRight: {x: right, y: bottom},
+      bottomLeft: {x: left, y: bottom}
+    } 
+  }
+
+  addBox(left, right, top, bottom) {
+    const points = this.getBoxPoints(left, right, top, bottom)
+
+    const lines = [
+      [points.topLeft, points.topRight],
+      [points.topRight, points.bottomRight],
+      [points.bottomRight, points.bottomLeft],
+      [points.bottomLeft, points.topLeft]
+    ]
+    this.lines.push(...lines);
+
+    for (const point in points) {
+      this.addPoint(points[point]);
+    }
+  }
+
+  /* Returns an array of lines. Each line is an array containing the start and
+  end point. The structure looks like this:
+  [
+    [{x: 10, y: 20}, {x: 30, y: 40}],    -- endpoints for line 1
+    [{x: 15, y: 25}, {x: 35, y: 45}],    -- endpoints for line 2
+    ...                                  -- etc.
+  ]
+  */
+  getHorizontalsFromLayer(layerData) {
+    const lines = [];
+    let left = undefined;
+    let right = undefined;
+
+    layerData.forEach((elem, index) => {
+      const row = Math.floor(index / mapData.width);
+      const column = index % mapData.width;      
+      if (elem != 0 &&
+        left == undefined &&
+        !layerData[index + mapData.width] &&
+        !layerData[index - mapData.width]) {
+        left = column * mapData.tilewidth;
+      }
+      // If there are tiles above and below, add intersection points.
+      if (left != undefined && (layerData[index + mapData.width] || layerData[index - mapData.width])) {
+        const left = column * mapData.tilewidth,
+              right = left + mapData.tilewidth,
+              top = row * mapData.tileheight,
+              bottom = top + mapData.tileheight;
+
+        const points = this.getBoxPoints(left, right, top, bottom);
+
+        for (const point in points) {
+          this.addPoint(points[point]);
+        }
+      }
+      if (left != undefined && (layerData[index + 1] == 0 || (index + 1) % mapData.width == 0)) {
+        right = column * mapData.tilewidth + mapData.tilewidth;
+
+        const top = row * mapData.tileheight,
+              bottom = top + mapData.tileheight;
+
+        this.addBox(left, right, top, bottom);
+
+        left = undefined;
+        right = undefined;
+      }
+    });
+    return lines;
+  }
+
+  /* Loops through the mapData matrix, column by column, checking for adjacent
+  vertical tiles. Returns an array of lines. See method above for the structure.
+  */
+  getVerticalsFromLayer(layerData) {
+    const lines = [];
+    let top = undefined;
+    let bottom = undefined;
+
+    const layerMatrix = convertArrayToMatrix(layerData, mapData.width, mapData.height);
+
+    let row = 0;
+    let column = 0;
+    let flag = false;
+
+    for (let k = 0; k < layerData.length; k++) {
+      row = k % mapData.height;
+      if (row == 0 && flag == true) {
+        column++;
+      }
+      flag = true;
+      const elem = layerMatrix[row][column];
+
+      if (elem != 0 && top == undefined && !(!layerMatrix[row + 1] || layerMatrix[row + 1][column] == 0)) {
+        top = row * mapData.tileheight;
+      }
+      if (top != undefined && (!layerMatrix[row + 1] || layerMatrix[row + 1][column] == 0)) {
+        bottom = row * mapData.tileheight + mapData.tileheight;
+
+        const left = column * mapData.tilewidth,
+              right = left + mapData.tilewidth;
+
+        this.addBox(left, right, top, bottom);
+
+        top = undefined;
+        bottom = undefined;
+      }
+    }
+    return lines;
+  }
+  // For debugging:
+  // drawLine(line) {
+  //   ctx.beginPath();
+  //   ctx.strokeStyle = 'green';
+  //   ctx.moveTo(line[0].x, line[0].y);
+  //   ctx.lineTo(line[1].x, line[1].y);
+  //   ctx.stroke();
+  // }
+
+  // render() {
+  //   for (const line of wireframe.lines) {
+  //     this.drawLine(line)
+  //   }
+  //   for (const point of wireframe.points) {
+  //     ctx.beginPath();
+  //     ctx.fillStyle = this.color;
+  //     ctx.rect(point.x, point.y, 5, 5);
+  //     ctx.fill();
+  //   }
+  // }
+}
+
+export { Wireframe };
--- src/js/util/util.js
+++ src/js/util/util.js
@@ -2,4 +2,56 @@ function angleToTarget(start, target,) {
   return (Math.atan2(target.y - start.y, target.x - start.x) / Math.PI) * 180;
 }
 
-export { angleToTarget };
+function radAngleToTarget(start, target) {
+  return Math.atan2(target.y - start.y, target.x - start.x);
+}
+
+function convertArrayToMatrix(arr, width, height) {
+  const matrix = [];
+  for (let i = 0; i < height; i++) {
+    matrix.push(arr.slice(i*width, i*width+width))
+  }
+  return matrix
+}
+
+/* 
+Get the intersection point of two lines. Return false if the lines don't
+intersect.
+*/ 
+function intersect(line1, line2) {
+  const x1 = line1[0].x,
+        y1 = line1[0].y,
+        x2 = line1[1].x,
+        y2 = line1[1].y,
+        x3 = line2[0].x,
+        y3 = line2[0].y,
+        x4 = line2[1].x,
+        y4 = line2[1].y
+
+  // Check that the lines are not length 0.
+	if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+		return false
+	}
+	const denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+  // Lines are parallel
+	if (denominator === 0) {
+		return false
+	}
+
+	const dist = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
+	const ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
+
+  // Check if the intersection is within the endpoints of the two lines.
+	if (dist < 0 || dist > 1 || ub < 0 || ub > 1) {
+		return false
+	}
+
+  // Return an object with the x, y coordinates of the intersection and the
+  // relative distance along the first line that the intersection occurred.
+	const x = x1 + dist * (x2 - x1)
+	const y = y1 + dist * (y2 - y1)
+
+	return {x, y, dist}
+}
+
+export { angleToTarget, radAngleToTarget, intersect, convertArrayToMatrix };
